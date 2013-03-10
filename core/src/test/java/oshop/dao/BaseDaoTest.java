64297083c80a830fe6587e7866be9aa2c2432302
sprilukin <sprilukin@gmail.com>
2013-03-10 12:14:03 +0100

    used dbcp

---
+++ core/src/test/java/oshop/dao/BaseDaoTest.java
@@ -0,0 +1,45 @@
+package oshop.dao;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.hibernate.SessionFactory;
+import org.junit.runner.RunWith;
+import org.springframework.orm.hibernate4.SessionFactoryUtils;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+import java.sql.Connection;
+
+import static org.junit.Assert.assertNotNull;
+
+@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:applicationContext-persistence.xml", "classpath:applicationContext-dao.xml"})
+@TransactionConfiguration(defaultRollback=true, transactionManager="transactionManager")
+public class BaseDaoTest {
+
+    @Resource
+    private SessionFactory sessionFactory;
+
+    protected void setUpDb(String pathToDataSet) throws Exception {
+        DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet(pathToDataSet));
+    }
+
+    private IDataSet getDataSet(String pathToDataSet) throws Exception {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(pathToDataSet);
+        return new FlatXmlDataSetBuilder().build(inputStream);
+    }
+
+    private IDatabaseConnection getConnection() throws Exception {
+        Connection jdbcConnection = SessionFactoryUtils.getDataSource(sessionFactory).getConnection();
+        return new DatabaseConnection(jdbcConnection);
+    }
+
+}
--- core/src/test/java/oshop/dao/CustomerDaoTest.java
+++ core/src/test/java/oshop/dao/CustomerDaoTest.java
@@ -1,36 +1,14 @@
 package oshop.dao;
 
-import org.dbunit.database.DatabaseConnection;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.operation.DatabaseOperation;
-import org.hibernate.SessionFactory;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.orm.hibernate4.SessionFactoryUtils;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 import oshop.model.Customer;
 
 import javax.annotation.Resource;
 
-import java.io.InputStream;
-import java.sql.Connection;
-
 import static org.junit.Assert.assertNotNull;
 
-@Transactional
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext-persistence.xml", "classpath:applicationContext-dao.xml"})
-@TransactionConfiguration(defaultRollback=true, transactionManager="transactionManager")
-public class CustomerDaoTest {
+public class CustomerDaoTest extends BaseDaoTest {
 
-
-    @Resource
-    private SessionFactory sessionFactory;
 
     @Resource
     private GenericDao<Customer, Integer> customerDao;
@@ -55,19 +33,4 @@ public class CustomerDaoTest {
 
         assertNotNull(customer);
     }
-
-    private void setUpDb(String pathToDataSet) throws Exception {
-        DatabaseOperation.CLEAN_INSERT.execute(getConnection(), getDataSet(pathToDataSet));
-    }
-
-    private IDataSet getDataSet(String pathToDataSet) throws Exception {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(pathToDataSet);
-        return new FlatXmlDataSetBuilder().build(inputStream);
-    }
-
-    private IDatabaseConnection getConnection() throws Exception {
-        Connection jdbcConnection = SessionFactoryUtils.getDataSource(sessionFactory).getConnection();
-        return new DatabaseConnection(jdbcConnection);
-    }
-
 }
