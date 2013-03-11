91fa9b95702dc217194208a3488e9fecfee05ded
sprilukin <sprilukin@gmail.com>
2013-03-11 10:50:09 +0100

    In progress

---
+++ web/src/main/java/oshop/api/ItemController.java
@@ -0,0 +1,9 @@
+package oshop.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/api/item")
+public class ItemController {
+}
