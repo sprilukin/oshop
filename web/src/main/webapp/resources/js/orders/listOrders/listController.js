b28c66309c47801e3ebc22fd93401a8d1907bd7f
sprilukin <sprilukin@gmail.com>
2013-05-27 22:58:15 +0200

    i18n for named filters in orders

--- web/src/main/webapp/resources/js/orders/listOrders/listController.js
+++ web/src/main/webapp/resources/js/orders/listOrders/listController.js
@@ -8,8 +8,9 @@ define([
     'orders/listOrders/listView',
     'common/baseListController',
     'common/advancedSearchView',
-    'common/filter'
-], function (_, Model, Collection, ListView, BaseListController, AdvancedSearchView, Filter) {
+    'common/filter',
+    'common/messages'
+], function (_, Model, Collection, ListView, BaseListController, AdvancedSearchView, Filter, messages) {
 
     var OrdersController = function() {
         var collection = new Collection();
@@ -21,18 +22,18 @@ define([
             View: ListView,
             filter: filter,
             searchView: new AdvancedSearchView({collection: collection, filter: filter, search: [
-                {field: "date", label: "Date equals"},
-                {field: "customer", label: "Customer like"},
-                {field: "productsCount", label: "Products count ="},
-                {field: "productsCountGE", label: "Products count >="},
-                {field: "productsCountLE", label: "Products count <="},
-                {field: "productsPrice", label: "Products price ="},
-                {field: "productsPriceGE", label: "Products price >="},
-                {field: "productsPriceLE", label: "Products price <="},
-                {field: "totalPrice", label: "Total price ="},
-                {field: "totalPriceGE", label: "Total price >="},
-                {field: "totalPriceLE", label: "Total price <="},
-                {field: "currentOrderStateName", label: "Status like"}
+                //{field: "date", label: messages["order_filter_date_eq"]},
+                {field: "customer", label: messages["order_filter_customer_like"]},
+                {field: "productsCount", label: messages["order_filter_productsCount_eq"]},
+                {field: "productsCountGE", label: messages["order_filter_productsCount_ge"]},
+                {field: "productsCountLE", label: messages["order_filter_productsCount_le"]},
+                {field: "productsPrice", label: messages["order_filter_productsPrice_eq"]},
+                {field: "productsPriceGE", label: messages["order_filter_productsPrice_ge"]},
+                {field: "productsPriceLE", label: messages["order_filter_productsPrice_le"]},
+                //{field: "totalPrice", label: messages["order_filter_totalPrice_eq"]},
+                //{field: "totalPriceGE", label: messages["order_filter_totalPrice_ge"]},
+                //{field: "totalPriceLE", label: messages["order_filter_totalPrice_le"]},
+                {field: "currentOrderStateName", label: messages["order_filter_currentOrderStateName_like"]}
             ]})
         });
     };
