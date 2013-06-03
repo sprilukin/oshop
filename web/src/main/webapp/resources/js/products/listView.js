b8ca157debdd63892ed34b15c3f48e585a32e200
sprilukin <sprilukin@gmail.com>
2013-06-03 20:03:17 +0200

    Added tiles view to products

--- web/src/main/webapp/resources/js/products/listView.js
+++ web/src/main/webapp/resources/js/products/listView.js
@@ -11,21 +11,25 @@ define([
     'common/sortView',
     'common/context',
     'text!templates/products/list.html',
+    'text!templates/products/tileList.html',
     'bootstrap'
-], function ($, _, Backbone, Mustache, imageGallery, messages, SortView, context, listEntityTemplate) {
+], function ($, _, Backbone, Mustache, imageGallery, messages, SortView, context, listEntityTemplate, tileListEntityTemplate) {
 
     return Backbone.View.extend({
 
         el: '.listEntities',
 
         events: {
-            "click a.delete": "delete"
+            "click a.delete": "delete",
+            "click #listType": "changeListType"
         },
 
         initialize: function(options) {
             this.collection.on("sync", function() {
                 this.render();
             }, this);
+
+            this.listTemplate = tileListEntityTemplate;
 
             this.sorterViews = [];
 
@@ -39,7 +43,7 @@ define([
 
         render: function () {
             var model = _.extend({context: context}, this.collection, messages);
-            this.$el.html(Mustache.render(listEntityTemplate, model));
+            this.$el.html(Mustache.render(this.listTemplate, model));
 
             _.each(this.sorterViews, function(view) {
                 view.render();
@@ -49,6 +53,12 @@ define([
         delete: function(event) {
             this.trigger("delete", {id: $(event.currentTarget).attr("data-id")});
             event.preventDefault();
+        },
+
+        changeListType: function(event) {
+            var checked = $(event.currentTarget).is(":checked");
+            this.listTemplate = checked ? tileListEntityTemplate : listEntityTemplate;
+            this.render();
         }
     });
 });
