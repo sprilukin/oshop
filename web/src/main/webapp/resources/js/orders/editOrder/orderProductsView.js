e5da4e41bfe807368db72862c56f04646350eb43
sprilukin <sprilukin@gmail.com>
2013-06-05 09:37:27 +0200

    Removed unnecessary stuff from order products list

--- web/src/main/webapp/resources/js/orders/editOrder/orderProductsView.js
+++ web/src/main/webapp/resources/js/orders/editOrder/orderProductsView.js
@@ -23,52 +23,17 @@ define([
         },
 
         initialize: function() {
-            this.model.on("error:addProduct", this.errorAddingProduct, this);
+            //this.model.on("error:addProduct", this.errorAddingProduct, this);
         },
 
         render: function () {
             var model = _.extend({context: context}, this.model.attributes, messages);
             this.$el.html(Mustache.render(listEntityTemplate, model));
-            this.errorPlaceHolder = $("#addProductContainer").find(".help-inline");
-
-            this.renderProductsSelect();
-        },
-
-        renderProductsSelect: function() {
-            this.productsSelect && this.productsSelect.destroy();
-            this.productsSelect = new DropDownWithSearch({
-                element: $("#field_products"),
-                placeholder: messages["order_select_product_placeholder"],
-                allowClear: true,
-                urlTemplate: context + "/api/products/filter;name={{term}};/sort;",
-                formatResult: function(data) {
-                    return Mustache.render(selectProductTemplate, _.extend({context: context}, data));
-                },
-                formatSelection: function(data) {
-                    return Mustache.render("{{id}} | {{name}}", data);
-                }
-            });
         },
 
         delete: function(event) {
             this.model.deleteProducts($(event.currentTarget).attr("data-id"));
             event.preventDefault();
-        },
-
-        add: function(event) {
-            this.errorPlaceHolder.text("");
-
-            var productId = $("#field_products").val();
-            if (!productId) {
-                this.errorPlaceHolder.text(messages["ui_validation_select_product"]);
-                return;
-            }
-
-            this.model.addProducts(productId);
-        },
-
-        errorAddingProduct: function(model, xhr) {
-            this.errorPlaceHolder.text(messages["ui_validation_select_product"]);
         }
     });
 });
