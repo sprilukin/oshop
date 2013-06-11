fa94b0873e121a370e2fb85b6fe8245a3afe193f
sprilukin <sprilukin@gmail.com>
2013-06-11 20:29:21 +0200

    Removed unnecessary method

--- web/src/main/webapp/resources/js/expenses/editView.js
+++ web/src/main/webapp/resources/js/expenses/editView.js
@@ -82,15 +82,6 @@ define([
             }
         },
 
-        getDate: function(dateAsString) {
-            var match = dateAsString.match(/([\d]{4})-([\d]{2})-([\d]{2})/);
-            if (match && match.length > 3) {
-                return new Date(match[1], match[2] - 1, match[3], 0, 0, 0, 0).getTime();
-            }
-
-            return "";
-        },
-
         onSubmit: function() {
             var that = this;
 
