3b681100511dba0a2e03fe53ff05a3ecbe9cc212
sprilukin <sprilukin@gmail.com>
2013-08-05 23:14:39 +0200

    r.js in progress

---
+++ web/src/main/config/app.config.js
@@ -0,0 +1,48 @@
+({
+    optimizeAllPluginResources: false,
+    appDir: "${basedir}/src/main/webapp/resources/js",
+    baseUrl: "./",
+    dir: "${project.build.directory}/${project.build.finalName}/resources/js",
+    paths: {
+        // Major libraries
+        jquery: 'lib/jquery-1.9.1',
+        underscore: 'lib/underscore',
+        backbone: 'lib/backbone',
+        mustache: 'lib/mustache',
+        bootstrap: 'lib/bootstrap',
+        select2: 'lib/select2',
+        cookies: 'lib/cookies',
+        datePicker: 'lib/bootstrap-datepicker',
+        datePickerRu: 'lib/bootstrap-datepicker.ru',
+        moment: 'lib/moment',
+        momentRu: 'lib/moment_ru',
+        //fileupload
+        fileupload: 'lib/fileup/jquery.fileupload',
+        iframeTransport: 'lib/fileup/jquery.iframe-transport',
+        jqueryUiWidget: 'lib/fileup/jquery.ui-widget',
+        //image-gallery
+        "load-image": 'lib/load-image',
+        "image-gallery": 'lib/bootstrap-image-gallery',
+        // Require.js plugins
+        text: 'lib/require.text',
+        templates: '../../templates',
+        messagesBase: '../../i18n/messages'
+    },
+    inlineText: false,
+    stubModules : ["messagesBase/ru", "messagesBase/en"],
+    modules: [
+        /*{name: "additionalPayments/main"},
+        {name: "addProductsToOrder/main"},
+        {name: "cities/main"},
+        {name: "customers/main"},
+        {name: "discounts/main"},
+        {name: "expenses/main"},
+        {name: "orders/editOrder/edit"},
+        {name: "orders/listOrders/list"},
+        {name: "orderStates/main"},
+        {name: "productCategories/main"},
+        {name: "products/main"},
+        {name: "shippingAddresses/main"},
+        {name: "shippingTypes/main"}*/
+    ]
+})
