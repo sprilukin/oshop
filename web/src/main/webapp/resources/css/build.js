5711b10f37779a0d3035f6d7d65a2bf85f8218aa
sprilukin <sprilukin@gmail.com>
2014-03-16 21:47:45 +0100

    Added special css optimization file

---
+++ web/src/main/webapp/resources/css/build.js
@@ -0,0 +1,14 @@
+/* Special CSS-only build file */
+({
+    appDir: ".",
+    baseUrl: ".",
+    removeCombined: true,
+    preserveLicenseComments: false,
+    //dir: "${project.build.directory}/${project.build.finalName}/resources",
+
+    optimizeCss: "standard",
+    cssImportIgnore: null,
+
+    paths: {
+    }
+})
