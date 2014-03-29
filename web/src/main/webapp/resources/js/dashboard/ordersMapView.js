ced39de891acf3afe0906a32f43b916249aafa67
sprilukin <sprilukin@gmail.com>
2014-03-29 12:49:49 +0100

    Added info widow

--- web/src/main/webapp/resources/js/dashboard/ordersMapView.js
+++ web/src/main/webapp/resources/js/dashboard/ordersMapView.js
@@ -6,9 +6,11 @@ define([
     'underscore',
     'backbone',
     'mustache',
+    "common/context",
     'orders/collection',
-    "googlemapsLoader"
-], function ($, _, Backbone, Mustache, Collection, mapsloader) {
+    "googlemapsLoader",
+    "text!dashboard/templates/infoWindowContentTemplate.html"
+], function ($, _, Backbone, Mustache, context, Collection, mapsloader, contentTemplate) {
 
     return Backbone.View.extend({
         el : "#googlemap",
@@ -17,7 +19,7 @@ define([
             _.bindAll(this, "render", "_initMap");
             this.collection = new Collection({customerId: null});
             this.filter = options.filter;
-            this.markers = [];
+            this.markers = {};
 
             mapsloader.done(this._initMap);
 
@@ -26,6 +28,8 @@ define([
         },
 
         render: function () {
+            this._clearMarkers();
+
             var self = this;
             _.each(this.collection.models, function(model) {
                 self._addMarker(model.attributes);
@@ -33,11 +37,14 @@ define([
         },
 
         _initMap: function(google) {
-            //http://maps.googleapis.com/maps/api/geocode/json?address=Васильковцы Тернопольская обл Украина&sensor=false&language=ru
-
+            this.google = google;
             this.map = new google.maps.Map($(this.el).get(0), {
                 center: new google.maps.LatLng(49.4333333, 32.0666667),
                 zoom: 6
+            });
+
+            this.infoWindow = new google.maps.InfoWindow({
+                content: ""
             });
         },
 
@@ -52,32 +59,69 @@ define([
                 return;
             }
 
-            var lat = parseFloat(city.latitude),
-                long = parseFloat(city.longitude),
-                position = new google.maps.LatLng(lat, long);
+            var key = city.name + ", " + city.region;
 
-            var marker = new google.maps.Marker({
+            if (!this.markers[key]) {
+                var lat = parseFloat(city.latitude),
+                    long = parseFloat(city.longitude),
+                    position = new this.google.maps.LatLng(lat, long);
+
+                this.markers[key] = this._createMarker(position, order);
+            } else {
+                this._addOrderToMarker(this.markers[key], order);
+            }
+        },
+
+        _createMarker: function(position, order) {
+            var marker = new this.google.maps.Marker({
                 position: position,
-                map: this.map,
-                title: city.name
+                map: this.map
             });
 
-            this.markers.push(marker);
+            var data = {
+                marker: marker,
+                orders: [order]
+            };
+
+            var self = this;
+            this.google.maps.event.addListener(marker, 'click', function() {
+                self.infoWindow.setContent(self._getInfoWindowContent(data.orders));
+                self.infoWindow.open(self.map, marker);
+            });
+
+            return data;
+        },
+
+        _addOrderToMarker: function(data, order) {
+            data.orders.push(order);
+        },
+
+        _getInfoWindowContent: function(orders) {
+            return Mustache.render(contentTemplate, {
+                orders: orders,
+                city: orders[0].shippingAddress.city,
+                context: context
+            });
         },
 
         _clearMarkers: function() {
             if (this.markers) {
-                for (var i = 0; i < this.markers.length; i++) {
-                    this.markers[i].setMap(null);
+                for (var key in this.markers) {
+                    if (this.markers.hasOwnProperty(key)) {
+                        var data = this.markers[key];
+                        this.google.maps.event.clearInstanceListeners(data.marker);
+                        data.marker.setMap(null);
+                    }
                 }
             }
 
-            this.markers = [];
+            this.infoWindow.close();
+            this.markers = {};
         },
 
         /* Event handlers */
+
         onCollectionSync: function() {
-            this._clearMarkers();
             this.render();
         },
 
