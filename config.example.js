ba2c3ea189787678385d0109ce530394fdf748ae
Sergey Prilukin <sprilukin@gmail.com>
2015-05-27 16:43:52 +0300

    initial checkins for nodejs env

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "database" : {
+    "dbName": "dbname",
+    "user": "username",
+    "password": "password",
+    "host": "localhost",
+    "dialect": "postgres",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "idle": 10000
+    }
+  }
+};
