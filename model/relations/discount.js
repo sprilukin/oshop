870959f7b093fc32082e980050ee3fb3dcf73224
Sergey Prilukin <sprilukin@gmail.com>
2015-06-01 12:30:09 +0300

    Model in progress

---
+++ model/relations/discount.js
@@ -0,0 +1,10 @@
+/**
+ * Discount types:
+ *  FIXED - fixed absolute discount
+ *  PERCENT - discount in percent
+ * @type {{FIXED: string, PERCENT: string}}
+ */
+module.exports = {
+    FIXED: "F",
+    PERCENT: "%"
+};
