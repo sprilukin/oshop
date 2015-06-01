88e22a83d6b22305d44f8b7ce6e3fc47b5aa79c9
Sergey Prilukin <sprilukin@gmail.com>
2015-06-01 13:53:57 +0300

    model in progress

--- db/sequelizeConfig.js
+++ db/sequelizeConfig.js
@@ -14,8 +14,8 @@ module.exports = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password, 
     freezeTableName: true,  //do not use plural table names
     timestamps: true,       //use timestamp tables to save timestamp when entity was created
                             //and last updated (true is default value)
-    createdAt: 'createdAt', //name of the createdAt table (createdAt is default)
-    updatedAt: 'updatedAt',  //name of the updatedAt table (updatedAt is default)
+    createdAt: 'created_at', //name of the createdAt table (createdAt is default)
+    updatedAt: 'updated_at',  //name of the updatedAt table (updatedAt is default)
 
     isolationLevel: Sequelize.REPEATABLE_READ
 });
