cc764d18b6ada33409074de838d17b4de568b63d
Sergey Prilukin <sprilukin@gmail.com>
2015-06-03 01:55:18 +0300

    tests in progress

--- test/ProductProductCategoryAssociations.js
+++ test/ProductProductCategoryAssociations.js
@@ -43,6 +43,18 @@ describe("Product - ProductCategory associations tests", function () {
         return Promise.all([pc, p1, p2]);
     };
 
+    var associate = function () {
+        return new Promise(function(resolve, reject) {
+            fillWithData().then(function(res) {
+                res[0].setProducts([res[1], res[2]]);
+
+                res[0].save().then(function (pc) {
+                    resolve([pc, res[1], res[2]]);
+                })
+            });
+        });
+    };
+
     it("Should be able to add products to ProductCategory", function (done) {
         fillWithData().then(function (res) {
             var pc = res[0],
@@ -85,4 +97,42 @@ describe("Product - ProductCategory associations tests", function () {
             })
         });
     });
+
+    it("Should be able to remove ProductCategory with associated product in it", function(done) {
+        associate().then(function(res) {
+            res[0].destroy().then(function() {
+                Product.findAll({include: [{
+                    model: ProductCategory,
+                    as: "ProductCategory"
+                }]}).then(function (products) {
+                    (products.length).should.be.equal(2);
+                    should(products[0].product_category_id).not.be.ok;
+                    should(products[1].product_category_id).not.be.ok;
+
+                    done();
+                });
+            })
+        })
+    });
+
+    xit("Should", function(done) {
+        associate().then(function(res) {
+            var updateAt1 = res[1].updatedAt,
+                updateAt2 = res[2].updatedAt;
+
+            res[0].update({
+                name: "name2"
+            }).then(function() {
+                Product.findAll({include: [{
+                    model: ProductCategory,
+                    as: "ProductCategory"
+                }]}).then(function (products) {
+                    var p1 = products[0].updatedAt,
+                        p2 = products[1].updatedAt;
+
+                    done();
+                });
+            })
+        })
+    })
 });
