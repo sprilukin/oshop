6582dce5c0291db1d93e2bfe4fc3b3b1ce9e894f
Sergey Prilukin <sprilukin@gmail.com>
2015-06-02 11:01:50 +0300

    tests in progress

--- test/ProductProductCategoryAssociations.js
+++ test/ProductProductCategoryAssociations.js
@@ -2,7 +2,8 @@ var should = require('should'),
     sequelize = require("../db/sequelize"),
     ProductCategory = require("../model/ProductCategory"),
     Product = require("../model/Product"),
-    dropQuery = require("config").get("dropQuery");
+    dropQuery = require("config").get("dropQuery"),
+    Promise = require("promise");
 
 describe("Product - ProductCategory associations tests", function () {
 
@@ -24,32 +25,64 @@ describe("Product - ProductCategory associations tests", function () {
         })
     });
 
-    it("Should create instance with specified name", function (done) {
-        ProductCategory.create({
+    var fillWithData = function () {
+        var pc = ProductCategory.create({
             name: "test"
-        }).then(function (pc) {
-            Product.create({
-                name: "product1",
-                price: 11.22
-            }).then(function (p1) {
-                Product.create(
-                    {
-                        name: "product2",
-                        price: 10
-                    }
-                ).then(function (p2) {
-                        pc.setProducts([p1, p2]).then(function () {
-                            pc.save().then(function(pc) {
-                                pc.getProducts().then(function(products) {
-                                    (products.length).should.be.equal(2);
-                                    (products[0]).name.should.be.equal("product1");
-                                    (products[1]).name.should.be.equal("product2");
-                                    done();
-                                });
-                            })
-                        })
-                    });
-            });
+        });
+
+        var p1 = Product.create({
+            name: "product1",
+            price: 11.22
+        });
+
+        var p2 = Product.create({
+            name: "product2",
+            price: 10
+        });
+
+        return Promise.all([pc, p1, p2]);
+    };
+
+    it("Should be able to add products to ProductCategory", function (done) {
+        fillWithData().then(function (res) {
+            var pc = res[0],
+                p1 = res[1],
+                p2 = res[2];
+
+            pc.setProducts([p1, p2]);
+            pc.save().then(function (pc) {
+                pc.getProducts().then(function (products) {
+                    (products.length).should.be.equal(2);
+                    (products[0]).name.should.be.equal("product1");
+                    (products[1]).name.should.be.equal("product2");
+                    done();
+                });
+            })
+        });
+    });
+
+    it("Should be able to add ProductCategory to products", function (done) {
+        fillWithData().then(function (res) {
+            var pc = res[0],
+                p1 = res[1],
+                p2 = res[2];
+
+            p1.setProductCategory(pc);
+            p2.setProductCategory(pc);
+
+            Promise.all([p1.save(), p2.save()]).then(function (res) {
+
+                Product.findAll({include: [{
+                    model: ProductCategory,
+                    as: "ProductCategory"
+                }]}).then(function (products) {
+                    (products.length).should.be.equal(2);
+                    (products[0].ProductCategory.id).should.be.equal(1);
+                    (products[1].ProductCategory.id).should.be.equal(1);
+
+                    done();
+                });
+            })
         });
     });
 });
